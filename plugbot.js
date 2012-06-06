39057d9d9febf04d521cfbd11ac3bb806eb639bf
Logic / Conner Davis <connergdavis@gmail.com>
2012-06-06 00:15:56 -0700

    Finished GUI w/ buttons: AUTOWOOT, AUTOMEH, AUTOQUEUE.  Fully functional.

--- plugbot.js
+++ plugbot.js
@@ -3,7 +3,10 @@
  * Purpose: Bookmarklet entry point
  */
 
+// Enable autoqueue?
 var autoqueue;
+
+// Enable autowoot? (Default is true)
 var autowoot = true;
  
  
@@ -16,7 +19,40 @@ function displayGUI() {
 	/* 
 	 * Generate the Plug.bot settings GUI.
 	 */
-	$("body").prepend("<div id=\"plugbot-gui\"></div>");
+	$(document).ready(function() {
+		if ($("#plugbot-gui").length) 
+			$("#plugbot-gui").remove();
+		$("#meta-frame").after('<div id="plugbot-gui"></div>');
+		$("#plugbot-gui").css("width", "348px").css("height", "48px").css("background-color", "#0A0A0A").css("opacity", "0.9100000262260437").css("border-top", "1px dotted #292929").css("-webkit-border-radius", "0 0 8px 8px").css("-moz-border-radius", "0 0 8px 8px").css("left", "349.5px").css("position", "absolute").css("top", "281px").css("z-index", "100");
+		
+		$("#plugbot-gui").prepend('<br /><span id="autowoot-btn">AUTOWOOT</span>');
+		$("#plugbot-gui").append('<span id="automeh-btn">AUTOMEH</span>');
+		$("#plugbot-gui").append('<span id="autoqueue-btn">AUTOQUEUE</span>');
+		$("#plugbot-gui span").css("font", "bold 12px arial").css("text-align", "center").css("margin-left", "32px").css("color", "#3FFF00").css("cursor", "pointer");
+		if (!autoqueue) 
+			$("#autoqueue-btn").css("color", "#ED1C24");
+		$("#plugbot-gui span:first-child").css("margin-left", "0px !important");
+		
+	});
+	$("#autowoot-btn").on('click', function() {
+		autowoot = !autowoot;
+		if (!autowoot) {
+			$(this).css("color", "#ED1C24");
+		} else {
+			$(this).css("color", "#3FFF00");
+			document.getElementById('button-vote-positive').click();
+		}
+	});
+	$("#autoqueue-btn").on('click', function() {
+		autoqueue = !autoqueue;
+		if (!autoqueue) {
+			$(this).css("color", "#ED1C24");
+			document.getElementById('button-dj-waitlist-leave').click();
+		} else {
+			$(this).css("color", "#3FFF00");
+			document.getElementById('button-dj-waitlist-join').click();
+		}
+	});
 }
 
 
@@ -59,8 +95,29 @@ function initListeners() {
 
 
 // Call init functions
+
+/*
+ * Determine if they want to enable auto-queue or not. 
+ * This may or may not be removed eventually.
+ */
 determineAutoqueue();
-document.getElementById('button-vote-positive').click();
+
+/*
+ * Display the UI that allows users to edit settings.
+ * This is just a small bar with icons that glow on/off.
+ */
+displayGUI();
+
+/*
+ * Click the Autowoot button as a start, if they want it.
+ * Same with the Wait List Join button.
+ */
+if (autowoot)
+	document.getElementById('button-vote-positive').click();
 if (autoqueue)
 	document.getElementById('button-dj-waitlist-join').click();
+	
+/*
+ * Init any listeners bound to the API.
+ */
 initListeners();
