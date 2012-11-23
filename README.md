2abf21e963b47d9f037e74661d419cd24cad90db
Conner Davis <connergdavis@gmail.com>
2012-11-23 00:07:43 -0800

    Update 1.0 ready for Plug.dj latest update as of nov 23

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ A collection of extensions to the growing online DJing website, <a href="plug.dj
 ### Features List ###
 
 + Auto-woot  :  Automatically WOOT! each song as it is played
-+ Auto-queueing  :  Automatically re-append yourself to the DJ Waitlist once you've been moved back to the crowd
++ Auto-queue/auto-DJ  :  Automatically re-append yourself to the DJ Waitlist once you've been moved back to the crowd
 + User list  :  A dynamic list of every user in the room, colour-coded based on their current vote.  Green = Woot, Red = Meh, White = No vote  Special usergroups are denoted by the icon you'd normally see in the chat
 + Button UI  :  Allows you to easily configure your Plug.bot experience;  enable or disable each of its features at the click of a button.  Red = Off, Green = On
 + Hide video  :  Allows you to hide videos from your view
-+ Custom username FX *NEW*  :  Now allows you to change some user's names in the chatbox to a different colour
++ Custom username FX  :  Now allows you to change some user's names in the chatbox to a different colour
 + Open-source code  :  All Plug.bot code is available in this repo, no cost, no secrecy
 
 
@@ -28,10 +28,9 @@ Alternatively, you can go<a href="http://pastebin.com/x8Xak4zU"> to this site </
 
 
 
-### Authors ###
+### Author ###
 
 + <strong>Conner Davis &lt;connergdavis@gmail.com&gt;</strong> <em>a.k.a. Logic</em>
-+ <strong>Harrison Schneidman &lt;hschneidman@gmail.com&gt;</strong> <em>a.k.a. EXƎ</em>
 
 
 
@@ -51,7 +50,7 @@ Try to keep the subject relevant, please.
 
 ### Version History ###
 
-<strong>0.1a (Released 6/6/2012)</strong>:
+<strong>0.1a // Released 6/6/2012</strong>:
 <ol>
 <li>Autowoot introduced</li>
 <li>Basic HTML UI</li>
@@ -60,7 +59,7 @@ Try to keep the subject relevant, please.
 
 
 
-<strong>0.2a (Released 29/6/2012)</strong>:
+<strong>0.2a // Released 29/6/2012</strong>:
 <ol>
 <li>Sebastian welcomes users as they join the room</li>
 <li>Introduced Woot/Meh Ratios List</li>
@@ -68,7 +67,7 @@ Try to keep the subject relevant, please.
 
 
 
-<strong>0.3a (Released 6/7/2012)</strong>:
+<strong>0.3a // Released 6/7/2012</strong>:
 <ol>
 <li>Woot/Meh Ratios nerfed, replaced with Userlist that properly updates and doesn't have ANY problems</li>
 <li>Clean UI with pictures instead of HTML text</li>
@@ -77,7 +76,7 @@ Try to keep the subject relevant, please.
 
 
 
-<strong>0.33a (Released 12/7/2012)</strong>:
+<strong>0.33a // Released 12/7/2012</strong>:
 <ol>
 <li>Userlist now recognises moderators and changes some styles for them.  First off, they are enbolden.  They also have the star next to their name, coloured to their vote.</li>
 <li>Patched a problem with moderators being the current DJ and the star being white.</li>
@@ -86,7 +85,7 @@ Try to keep the subject relevant, please.
 
 
 
-<strong>0.4a (Released 19/7/2012)</strong>:
+<strong>0.4a // Released 19/7/2012</strong>:
 <ol>
 <li>Userlist has been re-styled!  It now looks much sleeker</li>
 <li>Above the userlist is your current spot in the DJ waitlist, if you are in it.  This should make it easier to check instead of having to click the button and look for yourself</li>
@@ -96,11 +95,21 @@ Try to keep the subject relevant, please.
 
 
 
-<strong>0.5a (Released 01/8/2012)</strong>:
+<strong>0.5a // Released 01/8/2012</strong>:
 <ol>
 <li>Userlist font is smaller due to more users being in rooms.</li>
 <li>Hide video now lasts until you disable it, rather than resetting each time a new song comes around</li>
 <li>Moved the UI to be beside the chat for more space for buttons</li>
 <li>Buttons are now HTML-generated instead of pictures</li>
 <li>Custom username FX now allows you to highlight specific people with custom colours</li>
+</ol>
+
+
+
+<strong>1.0 // Released ??/11/2012</strong>:
+<ol>
+<li>Support's Plug.dj's newest release, Pepper!  Mostly rank changes</li>
+<li>Revamped userlist code, should be faster and 100% stable at this point.</li>
+<li>Userlist now allows you to @mention users by clicking their name.. can @mention multiple users as well</li>
+<li>The official release of Plug.bot; it is now "stable"!  Whatever that means!  More updates soon, everyone, thanks for sticking around :)  OVER 1,000 users strong!</li>
 </ol>
--- plugbot.js
+++ plugbot.js
@@ -61,7 +61,8 @@ var userList = true;
  */
 var customUsernames = new Array();
 
-/*
+
+/**
  * Initialise all of the Plug.dj API listeners which we use
  * to asynchronously intercept specific events and the data
  * attached with them. 
@@ -78,34 +79,37 @@ function initAPIListeners()
 	 * or Mehs the current song. 
 	 */
 	API.addEventListener(API.VOTE_UPDATE, function(obj) {
-		if (userList)
+		if (userList) {
 			populateUserlist();
+		}
 	});
 
 	/*
 	 * Whenever a user joins, this listener is called. 
 	 */
 	API.addEventListener(API.USER_JOIN, function(user) {
-		if (userList)
+		if (userList) {
 			populateUserlist();
+		}
 	});
 
 	/*
 	 * Called upon a user exiting the room. 
 	 */
 	API.addEventListener(API.USER_LEAVE, function(user) {
-		if (userList)
+		if (userList) {
 			populateUserlist();
+		}
 	});
 	
 	API.addEventListener(API.CHAT, checkCustomUsernames);
 }
 
 
-/*
+/**
  * Periodically check the chat history to see if any of the messages
  * match that of the user's chosen custom username FX.  If so, then we
- * need to stylise every instance of those. 
+ * need to style every instance of those. 
  */
 function checkCustomUsernames() 
 {
@@ -123,7 +127,7 @@ function checkCustomUsernames()
 }
 
 
-/*
+/**
  * Renders all of the Plug.bot "UI" that is visible beneath the video
  * player. 
  */
@@ -149,30 +153,24 @@ function displayUI()
 }
 
 
-/*
+/**
  * Prompt the user to provide a new custom username FX. 
+ * 
+ * TODO: Save to database per-user for saved settings.
  */
 function promptCustomUsername() {
 	var check = prompt("Format:  username:color\n(For color codes, Google 'Hexadecimal color chart')");
 	
 	customUsernames.push(check);
 	
-	$('#space').after('<span id="' + check + '" onclick="removeCustomUsername(\'' + check + '\');$(this).next().remove();$(this).remove();" style="cursor:pointer;color:#' + check.split(":")[1] + '">- ' + check.split(":")[0] 
+	$('#space').after('<span id="' + check + '" onclick="customUsernames.splice(\'' + check + '\', 1);$(this).next().remove();$(this).remove();" style="cursor:pointer;color:#' + check.split(":")[1] + '">- ' + check.split(":")[0] 
 		+ '</span><br />');
 		
 	checkCustomUsernames();
 }
 
 
-/*
- * Remove an existing entry in the custom username FX. 
- */
-function removeCustomUsername(data) {
-	customUsernames.splice(data, 1);
-}
-
-
-/*
+/**
  * For every button on the Plug.bot UI, we have listeners backing them
  * that are built to intercept the user's clicking each button.  Based 
  * on the button that they clicked, we can execute some logic that will
@@ -180,6 +178,9 @@ function removeCustomUsername(data) {
  */
 function initUIListeners()
 {	
+	/*
+	 * Toggle userlist.
+	 */
 	$("#plugbot-btn-userlist").on("click", function() {
 		userList = !userList;
 		$(this).css("color", userList ? "#3FFF00" : "#ED1C24");
@@ -191,6 +192,9 @@ function initUIListeners()
 		}
 	});
 
+	/*
+	 * Toggle auto-woot.
+	 */
 	$("#plugbot-btn-woot").on("click", function() {
 		autowoot = !autowoot;
 		$(this).css("color", autowoot ? "#3FFF00" : "#ED1C24");
@@ -198,6 +202,9 @@ function initUIListeners()
 			$("#button-vote-positive").click();
 	});
 
+	/*
+	 * Toggle hide video.
+	 */
 	$("#plugbot-btn-hidevideo").on("click", function() {
 		hideVideo = !hideVideo;
 		$(this).css("color", hideVideo ? "#3FFF00" : "#ED1C24");
@@ -205,6 +212,9 @@ function initUIListeners()
 		$("#playback .frame-background").animate({"opacity": (hideVideo ? "0" : "0.91")}, {duration: "medium"});
 	});
 
+	/*
+	 * Toggle auto-queue/auto-DJ.
+	 */
 	$("#plugbot-btn-queue").on("click", function() {
 		autoqueue = !autoqueue;
 		$(this).css("color", autoqueue ? "#3FFF00" : "#ED1C24");
@@ -212,11 +222,12 @@ function initUIListeners()
 	});
 }
 
-/*
+
+/**
  * Called whenever a new DJ begins playing in the room.
  *  
- * @param {Object} obj
- * 				This contains the user (current DJ)'s data.
+ * @param obj
+ * 				This contains the current DJ's data.
  */
 function djAdvanced(obj) 
 {
@@ -232,24 +243,28 @@ function djAdvanced(obj)
 	/*
 	 * If auto-woot is enabled, WOOT! the song.
 	 */
-	if (autowoot) 
+	if (autowoot) {
 		$("#button-vote-positive").click();
+	}
 
 	/*
 	 * If auto-queueing has been enabled, and they have just recently
 	 * left the waitlist, join them back.
 	 */
-	if ($("#button-dj-waitlist-join").css("display") === "block" && autoqueue)
-		$("#button-dj-waitlist-join").click();
+	if ($("#button-dj-waitlist-join").css("display") === "block" && autoqueue) {
+		$("#button-dj-waitlist-join").click();	
+	}
 	
 	/*
 	 * If the userlist is enabled, re-populate it.
 	 */
-	if (userList)
+	if (userList) {
 		populateUserlist();
+	}
 }
 
-/*
+
+/**
  * Generates every user in the room and their current vote as 
  * colour-coded text.  Also, moderators get the star next to
  * their name. 
@@ -267,6 +282,12 @@ function populateUserlist()
 	 */
 	$('#plugbot-userlist').append('<h1 style="text-indent:12px;color:#42A5DC;font-size:14px;font-variant:small-caps;">Users: ' + API.getUsers().length + '</h1>');
 
+	/*
+	 * Disclaimer that yes, you can now mention people from the
+	 * userlist!
+	 */
+	$('#plugbot-userlist').append('<p style="padding-left:12px;text-indent:0px !important;font-style:italic;color:#42A5DC;font-size:11px;">Click a username to<br />@mention them! *NEW</p><br />');
+	
 	/*
 	 * If the user is in the waitlist, show them their current spot.
 	 */
@@ -305,12 +326,12 @@ function populateUserlist()
 }
 
 
-/*
+/**
  * Appends another user's username to the userlist.
  *  
- * @param {Object} username
+ * @param username
  * 				The username of this user.
- * @param {Object} vote
+ * @param vote
  * 				Their current 'vote', which may be:
  * 					-1 	: Meh
  *					0	: 'undecided' (hasn't voted yet)
@@ -318,85 +339,152 @@ function populateUserlist()
  */
 function appendUser(user) 
 {
-	var username 	= user.username;
-	var vote 		= user.vote;
+	var username = user.username;
+	/*
+	 * A new feature to Pepper, which is a permission value,
+	 * may be 1-5 afaik.  
+	 * 
+	 * 1: normal (or 0)
+	 * 2: bouncer
+	 * 3: manager
+	 * 4/5: (co-)host
+	 */
+	var permission = user.permission;
 	
 	/*
-	 * Some variables that we'll either be setting as true/false
-	 * (some conditionals that do major changes to their look in the userlist)
-	 * or setting a value to.
+	 * If they're an admin, set them as a fake permission,
+	 * makes it easier.  
 	 */
-	var colour;
-	var currentDj = false;
-	var moderator = user.moderator;
-	if (API.getSuperUsers() != null) 	var su = user.superuser;
-	if (API.getHost() != null) 			var host = user.owner;
-	var img;
-
+	if (user.admin) {
+		permission = 99;
+	}
+	
 	/*
-	 * Based on their vote, apply the colour coding.
+	 * For special users, we put a picture of their rank
+	 * (the star) before their name, and colour it based
+	 * on their vote.
 	 */
-	switch (vote) 
-	{
-		case 1:		// WOOT!
-			colour = "3FFF00";
-			if (moderator)
-				img = "http://i.imgur.com/T5G4f.png";
-			if (host)
-				img = "http://i.imgur.com/Lu1qo.png";
-			if (su)
-				img = "http://i.imgur.com/XA1DE.png";
+	var imagePrefix;
+	switch (permission) {
+		case 0:		// Normal user
+		case 1:		// Featured DJ
+			imagePrefix = 'normal';
 			break;
-		case 0:		// Undecided
-			colour = "FFFFFF";
-			if (moderator) 
-				img = "http://i.imgur.com/sRsU0.png";
-			if (host)
-				img = "http://i.imgur.com/6Bq5W.png";
-			if (su)
-				img = "http://i.imgur.com/veoVS.png";
+		case 2:		// Bouncer
+			imagePrefix = 'bouncer';
 			break;
-		case -1:	// Meh
-			colour = "ED1C24";
-			if (moderator)
-				img = "http://i.imgur.com/JPA1Q.png";
-			if (host)
-				img = "http://i.imgur.com/wVLR3.png";
-			if (su)
-				img = "http://i.imgur.com/5LcI3.png";
+		case 3:		// Manager
+			imagePrefix = 'manager';
+			break;
+		case 4:
+		case 5: 	// Co-host
+			imagePrefix = 'host';
+			break;
+		case 99:	// Admin
+			imagePrefix = 'admin';
 			break;
 	}
-
+	
 	/*
-	 * If they're the current DJ, apply some more special
-	 * changes.
+	 * If they're the current DJ, override their rank
+	 * and show a different colour, a shade of blue, 
+	 * to denote that they're playing right now (since
+	 * they can't vote their own song.)
 	 */
-	if (API.getDJs().length > 0 && API.getDJs()[0].username == username) {
-		currentDj = true;
-		colour = "42A5DC";
-		if (moderator)
-			img = "http://i.imgur.com/CsK3d.png";
+	if (API.getDJs()[0].username == username) {
+		if (imagePrefix === 'normal') {
+			drawUserlistItem('void', '#42A5DC', username);
+		} else {
+			drawUserlistItem(imagePrefix + '_current.png', '#42A5DC', username);
+		}
+	} else if (imagePrefix === 'normal') {
+		/*
+		 * If they're a normal user, they have no special icon.
+		 */
+		drawUserlistItem('void', colorByVote(user.vote), username);
+	} else {
+		/*
+		 * Otherwise, they're ranked and they aren't playing,
+		 * so draw the image next to them.
+		 */
+		drawUserlistItem(imagePrefix + imagePrefixByVote(user.vote), colorByVote(user.vote), username);
 	}
+}
 
+
+/**
+ * Determine the color of a person's username in the
+ * userlist based on their current vote.
+ * 
+ * @param vote
+ * 				Their vote: woot, undecided or meh.
+ */
+function colorByVote(vote) {
+	if (!vote)	{
+		return '#fff'; // blame Boycey
+	}
+	switch (vote) {
+		case -1: 	return '#c8303d';
+		case 0:		return '#fff';
+		case 1:		return '#c2e320';
+	}
+}
+
+
+/**
+ * Determine the "image prefix", or a picture that
+ * shows up next to each user applicable in the userlist.
+ * This denotes their rank, and its color is changed
+ * based on that user's vote.
+ * 
+ * @param vote
+ * 				Their current vote.
+ * @returns
+ * 				The varying path to the PNG image for this user,
+ * 				as a string.  NOTE:  this only provides the suffix
+ * 				of the path.. the prefix of the path, which is 
+ * 				admin_, host_, etc. is done inside {@link #appendUser(user)}.
+ */
+function imagePrefixByVote(vote) {
+	if (!vote) {
+		return '_undecided.png'; // blame boycey again
+	}
+	switch (vote) {
+		case -1:	return '_meh.png';
+		case 0:		return '_undecided.png';
+		case 1:		return '_woot.png';
+	}
+}
+
+
+/**
+ * Draw a user in the userlist.
+ * 
+ * @param imagePath
+ * 				An image prefixed by their username denoting 
+ * 				rank; bouncer/manager/etc. 'void' for normal users.
+ * @param color
+ * 				Their color in the userlist, based on vote.
+ * @param username
+ * 				Their username. 
+ */
+function drawUserlistItem(imagePath, color, username) {
 	/*
-	 * Sometimes undecided mod star breaks.  This fixes that.
+	 * If they aren't a normal user, draw their rank icon.
 	 */
-	if (img == undefined && (moderator || host || su)) 
-	{
-		colour = "FFFFFF";
-		img = moderator ? "http://i.imgur.com/sRsU0.png" : "http://i.imgur.com/6Bq5W.png";
+	if (imagePath !== 'void') {
+		var realPath = 'http://www.theedmbasement.com/basebot/userlist/' + imagePath;
+		$('#plugbot-userlist').append('<img src="' + realPath + '" align="left" style="margin-left:6px" />');
 	}
-
+	
 	/*
-	 * Apply the HTML code to the page that actually displays them
-	 * inside the userlist.
+	 * Write the HTML code to the userlist.
 	 */
 	$('#plugbot-userlist').append(
-		((moderator || host || su) ? '<img src="' + img + '" align="left" style="margin-left:6px" alt="Moderator" />' : '') 
-		+ '<p style="' + ((moderator || host || su) ? 'text-indent:6px !important;' : '') 
-		+ 'color:#' + colour + ';' + (currentDj ? 'font-weight:bold;font-size:15px' : '') + '"' 
-		+ (currentDj ? ('title="' + username + ' is the current DJ!"') : '') + '>' 
-		+ username + '</p>'
+		'<p style="cursor:pointer;' + (imagePath === 'void' ? '' : 'text-indent:6px !important;')
+		+ 'color:' + color + ';'
+		+ ((API.getDJs()[0].username == username) ? 'font-size:15px;font-weight:bold;' : '')
+		+ '" onclick="$(\'#chat-input-field\').val($(\'#chat-input-field\').val() + \'@' + username + ' \').focus();">' + username + '</p>'
 	);
 }
 
@@ -412,11 +500,6 @@ $('#plugbot-userlist').remove();
 $('#plugbot-css').remove();
 $('#plugbot-js').remove();
 
-/*
- * Implement jQuery UI (some of it's included in Plug, but not
- * what we need, which is Resizable.) 
- */
-$('body').prepend('<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>');
 
 /*
  * Write the CSS rules that are used for components of the 
@@ -427,7 +510,7 @@ $('body').prepend('<style type="text/css" id="plugbot-css">'
 	+ '#plugbot-ui p { background-color: #0b0b0b; height: 32px; padding-top: 8px; padding-left: 8px; cursor: pointer; font-variant: small-caps; width: 84px; font-size: 15px; margin: 0; }'
 	+ '#plugbot-ui h2 { background-color: #0b0b0b; height: 112px; width: 156px; margin: 0; color: #fff; font-size: 13px; font-variant: small-caps; padding: 8px 0 0 12px; border-top: 1px dotted #292929; }'
     + '#plugbot-userlist { border: 6px solid rgba(10, 10, 10, 0.8); border-left: 0 !important; background-color: #000000; padding: 8px 0px 20px 0px; width: 12%; }'
-    + '#plugbot-userlist p { margin: 0; padding-top: 2px; text-indent: 24px; font-size: 10px; }'
+    + '#plugbot-userlist p { margin: 0; padding-top: 4px; text-indent: 24px; font-size: 10px; }'
     + '#plugbot-userlist p:first-child { padding-top: 0px !important; }'
     + '#plugbot-queuespot { color: #42A5DC; text-align: left; font-size: 15px; margin-left: 8px }');
 
